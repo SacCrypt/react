2c0b4737b19117b26d1da08fb01c6ebc1a7a11ec
SacCrypt <sachinfulsunge1@gmail.com>
2023-03-29 15:05:11 +0530

    update

--- materialui/src/components/MuiTypography.tsx
+++ materialui/src/components/MuiTypography.tsx
@@ -8,6 +8,7 @@ import {
   ToggleButtonGroup,
   InputAdornment,
   Box,
+  Menu,
   MenuItem,
   FormControl,
   FormLabel,
@@ -21,9 +22,41 @@ import {
   Autocomplete,
   Divider,
   Grid,
+  Paper,
+  Card,
+  CardContent,
+  CardActions,
+  CardMedia,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  AppBar,
+  Toolbar,
+  Link,
+  Breadcrumbs,
+  Drawer,
+  SpeedDial,
+  SpeedDialAction,
+  SpeedDialIcon,
+  BottomNavigation,
+  BottomNavigationAction,
+  Avatar,
+  AvatarGroup,
+  Badge,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  ListItemAvatar,
+  ListItemButton,
 } from "@mui/material";
 import {
+  Mail,
   Send,
+  NavigateNext,
   FormatBold,
   FormatItalic,
   FormatUnderlined,
@@ -31,8 +64,17 @@ import {
   BookmarkBorder,
   Favorite,
   FavoriteBorder,
+  ExpandMore,
+  CatchingPokemon,
+  KeyboardArrowDown,
+  Share,
+  Print,
+  CopyAll,
+  Edit,
+  Home,
+  Person,
 } from "@mui/icons-material";
-
+import MenuIcon from "@mui/icons-material/Menu";
 import React, { useState } from "react";
 
 export const MuiTypography = () => {
@@ -50,6 +92,34 @@ export const MuiTypography = () => {
   const [rating, setRating] = useState<number | null>(2.5);
   const [organ, setOrgan] = useState<string | null>(null);
   const organs = ["Dick", "Bawls", "coochie", "boobies", "armpithair"];
+  const [expanded, setExpanded] = useState<string | false>(false);
+  const [anchor, setAnchor] = useState<null | HTMLElement>(null);
+  const [drawer, setDrawer] = useState(false);
+  const [bottom, setBottom] = useState(0);
+  const open = Boolean(anchor);
+
+  const images = [
+    {
+      img: "https://example.com/image1.jpg",
+      title: "Beautiful sunset at the beach",
+    },
+    {
+      img: "https://example.com/image2.jpg",
+      title: "Snowy mountains in winter",
+    },
+    {
+      img: "https://example.com/image3.jpg",
+      title: "Vibrant city skyline at night",
+    },
+    {
+      img: "https://example.com/image4.jpg",
+      title: "Adorable puppy playing in the grass",
+    },
+    {
+      img: "https://example.com/image5.jpg",
+      title: "Colorful hot air balloons in the sky",
+    },
+  ];
   return (
     <div>
       <Stack direction="row" spacing={2}>
@@ -243,23 +313,333 @@ export const MuiTypography = () => {
           },
         }}
       >
-        Itchy Sweaty Balls
+        Scatchy itchy balls
       </Box>
-      <Grid my={4} container>
-        <Grid item xs sm={6}>
-          <Box bgcolor="primary.light">item 1</Box>
+      <Paper sx={{ padding: "32px" }} elevation={4}>
+        <Grid my={4} container>
+          <Grid item xs sm={6}>
+            <Box bgcolor="primary.light">item 1</Box>
+          </Grid>
+          <Grid item xs={6}>
+            {" "}
+            <Box bgcolor="primary.light">item 2</Box>
+          </Grid>
+          <Grid item xs="auto">
+            <Box bgcolor="primary.light">item 3</Box>
+          </Grid>
+          <Grid item xs>
+            <Box bgcolor="primary.light">item 4</Box>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          {" "}
-          <Box bgcolor="primary.light">item 2</Box>
-        </Grid>
-        <Grid item xs="auto">
-          <Box bgcolor="primary.light">item 3</Box>
-        </Grid>
-        <Grid item xs>
-          <Box bgcolor="primary.light">item 4</Box>
-        </Grid>
-      </Grid>
+      </Paper>
+
+      <Box width="300px">
+        <Card>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://source.unsplash.com/random"
+            alt="unsplash image"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              React
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+              doloremque harum cum dolore rem eaque repudiandae facere nostrum
+              tempora et vel, quis commodi, hic facilis dignissimos suscipit,
+              expedita odit sit.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small"> Share </Button>
+            <Button size="small"> Learn More</Button>
+          </CardActions>
+        </Card>
+      </Box>
+      <div>
+        <Accordion expanded={expanded === "panel1"}>
+          <AccordionSummary
+            id="panel1-header"
+            aria-controls="panel1-content"
+            expandIcon={<ExpandMore />}
+          >
+            {" "}
+            <Typography>Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {" "}
+            <Typography>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet
+              beatae voluptatum hic saepe veniam tenetur eius iure molestiae
+              minus. Excepturi magnam modi nesciunt id quae. Facilis dicta
+              temporibus id praesentium!
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion expanded={expanded === "panel2"}>
+          <AccordionSummary
+            id="panel2-header"
+            aria-controls="panel2-content"
+            expandIcon={<ExpandMore />}
+          >
+            {" "}
+            <Typography>Accordion 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {" "}
+            <Typography>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet
+              beatae voluptatum hic saepe veniam tenetur eius iure molestiae
+              minus. Excepturi magnam modi nesciunt id quae. Facilis dicta
+              temporibus id praesentium!
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Stack>
+          <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+            {images.map((item) => (
+              <ImageListItem key={item.img}>
+                <img
+                  src={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2`}
+                  alt="itemtitle"
+                  loading="lazy"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+          <ImageList
+            variant="woven"
+            sx={{ width: 500, height: 450 }}
+            cols={3}
+            rowHeight={164}
+          >
+            {images.map((item) => (
+              <ImageListItem key={item.img}>
+                <img
+                  src={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2`}
+                  alt="itemtitle"
+                  loading="lazy"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+          <ImageList
+            variant="masonry"
+            sx={{ width: 500, height: 450 }}
+            cols={3}
+            rowHeight={164}
+          >
+            {images.map((item) => (
+              <ImageListItem key={item.img}>
+                <img
+                  src={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2`}
+                  alt="itemtitle"
+                  loading="lazy"
+                />
+                <ImageListItemBar title={item.title} />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </Stack>
+      </div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="logo"
+          >
+            <CatchingPokemon />
+          </IconButton>
+          <Typography sx={{ flexGrow: 1 }} component="div" variant="h6">
+            Pokemon negros
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            <Button color="inherit">Features</Button>
+            <Button color="inherit">Pricing</Button>
+            <Button color="inherit">About</Button>
+            <Button color="inherit">Login</Button>
+            <Button
+              id="resources-button"
+              onClick={(e) => setAnchor(e.currentTarget)}
+              color="inherit"
+              aria-controls={open ? "resources-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              endIcon={<KeyboardArrowDown />}
+            >
+              Resources
+            </Button>
+          </Stack>
+          <Menu
+            MenuListProps={{
+              "aria-labelledby": "resources-button",
+            }}
+            open={open}
+            anchorEl={anchor}
+            id="resources-menu"
+            onClose={() => {
+              setAnchor(null);
+            }}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+          >
+            <MenuItem
+              onClick={() => {
+                setAnchor(null);
+              }}
+            >
+              Blog
+            </MenuItem>
+            <MenuItem>Podcast</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+      <Stack spacing={2} direction="row" m={4}>
+        <Typography>
+          <Link variant="body2" href="#" color="secondary" underline="hover">
+            Link
+          </Link>
+        </Typography>
+      </Stack>
+      <Box m={2}>
+        <Breadcrumbs
+          separator={<NavigateNext fontSize="small" />}
+          aria-label="breadcrumb"
+          maxItems={2}
+          itemsAfterCollapse={2}
+          itemsBeforeCollapse={1}
+        >
+          <Link href="#">Home</Link>
+          <Link href="#">Catalog</Link>
+          <Link href="#">Accessories</Link>
+          <Typography color="text.primary">Shoes</Typography>
+        </Breadcrumbs>
+      </Box>
+      <>
+        <IconButton
+          onClick={() => setDrawer(true)}
+          size="large"
+          edge="start"
+          color="inherit"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer open={drawer} onClose={() => setDrawer(false)} anchor="left">
+          <Box p={2} width="250px" textAlign="center" role="presentation">
+            <Typography variant="h6" component="div">
+              Side Panel
+            </Typography>
+          </Box>
+        </Drawer>
+        <SpeedDial
+          ariaLabel="Navigation speed Dial"
+          sx={{ position: "absolute", bottom: 16, right: 16 }}
+          icon={<SpeedDialIcon openIcon={<Edit />} />}
+        >
+          <SpeedDialAction tooltipOpen icon={<CopyAll />} tooltipTitle="Copy" />
+          <SpeedDialAction icon={<Share />} tooltipTitle="Share" />
+          <SpeedDialAction icon={<Print />} tooltipTitle="Print" />
+        </SpeedDial>
+      </>
+      <BottomNavigation
+        value={bottom}
+        onChange={(event, newValue) => setBottom(newValue)}
+        sx={{ width: "100%", position: "absolute", bottom: 0 }}
+        showLabels
+      >
+        <BottomNavigationAction label="Home" icon={<Home />} />
+        <BottomNavigationAction label="Favourite" icon={<Favorite />} />
+        <BottomNavigationAction label="Profile" icon={<Person />} />
+      </BottomNavigation>
+
+      <Stack spacing={4}>
+        <Stack direction="row" spacing={1}>
+          <AvatarGroup max={3}>
+            <Avatar sx={{ width: 48, height: 48, bgcolor: "primary.light" }}>
+              Cotton
+            </Avatar>
+            <Avatar variant="square" sx={{ bgcolor: "success.light" }}>
+              Wool
+            </Avatar>
+            <Avatar
+              variant="rounded"
+              src="https://randomuser.me/api/portraits/women/79.jpg"
+              alt="Junidher"
+            />
+            <Avatar
+              src="https://randomuser.me/api/portraits/men/51.jpg"
+              alt="Janab"
+            />
+          </AvatarGroup>
+        </Stack>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Badge badgeContent={5} color="primary">
+          <Mail />
+        </Badge>
+        <Badge max={999} showZero badgeContent={100} color="secondary">
+          <Mail />
+        </Badge>
+        <Badge
+          invisible={true}
+          variant="dot"
+          max={999}
+          showZero
+          badgeContent={100}
+          color="secondary"
+        >
+          <Mail />
+        </Badge>
+      </Stack>
+
+      <Box sx={{ width: "400px", bgcolor: "#efefef" }}>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <ListItemAvatar>
+                  <Avatar>
+                    <Mail />
+                  </Avatar>
+                </ListItemAvatar>
+              </ListItemIcon>
+              <ListItemText secondary="Secondary Text" primary="List item 1" />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemIcon>
+              <ListItemAvatar>
+                <Avatar>
+                  <Mail />
+                </Avatar>
+              </ListItemAvatar>
+            </ListItemIcon>
+            <ListItemText secondary="Secondary Text" primary="List item 2" />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <ListItemAvatar>
+                <Avatar>
+                  <Mail />
+                </Avatar>
+              </ListItemAvatar>
+            </ListItemIcon>
+            <ListItemText secondary="Secondary Text" primary="List item 3" />
+          </ListItem>
+        </List>
+      </Box>
     </div>
   );
 };
